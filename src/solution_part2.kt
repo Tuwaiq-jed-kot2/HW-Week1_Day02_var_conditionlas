99cd913ce4be5d5aa0f42de7bb6df0fdb126b5b3
Sumaya Altuwairqi <91243918+altuwairqi-sa@users.noreply.github.com>
2021-09-28 23:05:14 +0300

    thw homework is great but I think it was a bit easy, + it was controlled a lot it didn't have chance for creativity. for example I think it would be better to ask us for two variables and use condetionals us we like

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,58 @@
 fun main(){
- print("I love Kotlin!!!!")
+    val player1 = 43
+    val player2 = 76
+    if (player1 in 90..100) {
+        println ("player1 health is full")
+    }
+    else if (player1 in 50..89){
+        println ("player1 health is fine")
+    }
+    else if (player1 in 0..49){
+        println ("player1 health is awful")
+    }
+    else println ("wrong input")
+
+    if (player2 in 90..100) {
+        println ("player2 health is full")
+    }
+    else if (player2 in 50..89){
+        println ("player2 health is fine")
+    }
+    else if (player2 in 0..49){
+        println ("player2 health is awful")
+    }
+    else println ("wrong input")
+
 }
+
+
+/* side note
+the home work could've been done using while as shown below
+
+when (player1) {
+        in 90..100 -> {
+            println ("player1 health is full")
+        }
+        in 50..89 -> {
+            println ("player1 health is fine")
+        }
+        in 0..49 -> {
+            println ("player1 health is awful")
+        }
+        else -> println ("wrong input")
+    }
+
+    when (player2) {
+        in 90..100 -> {
+            println ("player2 health is full")
+        }
+        in 50..89 -> {
+            println ("player2 health is fine")
+        }
+        in 0..49 -> {
+            println ("player2 health is awful")
+        }
+        else -> println ("wrong input")
+    }
+
+ */
