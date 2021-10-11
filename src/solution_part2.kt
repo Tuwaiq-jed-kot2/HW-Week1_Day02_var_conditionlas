ae5697fb6d009f9341d63fbd34f89a3d10f35016
BashayerH <Bashayer.Ismaeel.jed.k2@tuwaiq.edu.sa>
2021-10-11 23:03:22 +0300

    not that easy but i enjoyed solve it

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,18 @@
 fun main(){
- print("I love Kotlin!!!!")
+ val player1= 80
+var healthPointP1= if (player1 in 50..90) {
+  "Player1 you have $player1 health points so your health point is fine!!"
+}else{
+ " Player1 you have $player1 health points so your health point is awful!!"
+}
+ println(healthPointP1)
+
+    val player2= 30
+    var healthPointP2= if (player2 in 50..90) {
+        "Player2 you have $player2 health points so your health point is fine!!"
+    }else{
+        "Player2 you have $player2 health points so your health point is awful!!"
+    }
+    println(healthPointP2)
+
 }
