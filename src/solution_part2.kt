70e4eb85c8337b4ff7204b57aa5d9c727674643c
EmanSamkri <Eman.Jameel.jed.k2@tuwaiq.edu.sa>
2021-10-12 00:40:16 +0300

    Great

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,22 @@
 fun main(){
- print("I love Kotlin!!!!")
+ val healthPoints_player1 = 85
+ val healthPoints_player2 = 49
+  print("The first player has health points = $healthPoints_player1 so, ")
+     when (healthPoints_player1) {
+         in 90..100 -> println ("the player's health is normal")
+         in 50..89 -> println ("the player's health is fine")
+         in 0..49 -> println ("the player's health is awful")
+         else -> println ("Error: the health point must be in the range of 0 to 100")
+     }
+
+    print("the second player has health points = $healthPoints_player2 so, ")
+    when (healthPoints_player2) {
+        in 90..100 -> println ("the player's health is normal")
+        in 50..89 -> println ("the player's health is fine")
+        in 0..49 -> println ("the player's health is awful")
+        else -> println ("Error: the health point must be in the range of 0 to 100")
+    }
+
+
+
 }
