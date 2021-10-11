cbfab6b21db30ac82c822ba9c4fba7cf6dd845ec
SomyahAA <somyah.abdulaziz.jed.k2@tuwaiq.edu.sa>
2021-10-12 01:16:01 +0300

    Great start

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,39 @@
 fun main(){
- print("I love Kotlin!!!!")
+
+ val player1HealthPoints= 40
+ val player2HealthPoints= 60
+
+ if (player1HealthPoints < 50) {
+  println("the player's health is awful")
+ }
+ else if (player1HealthPoints < 90){
+  println("the player's health is fine")
+ }
+
+ if (player2HealthPoints  < 50) {
+  println("the player's health is awful")
+ }
+ else if (player2HealthPoints < 90) {
+  println("the player's health is fine")
+ }
+
+// I thought writing this code with function will be better, so I gave it a try . I hope  both of them are correct
+
+ /*
+ val player1HealthPoints= 40
+ val player2HealthPoints= 60
+
+
+ fun healthCondition(healthPoints: Int){
+     if (healthPoints < 50) {
+         println("the player's health is awful")
+     }
+     else if (healthPoints < 90){
+         println("the player's health is fine")
+     }
+ }
+
+ healthCondition(player1HealthPoints)
+ healthCondition(player2HealthPoints)
+*/
 }
