438b08dacd3aa7c098352c666bc0050c37067919
Azzam <Azzam.Abdullah.jed.k2@tuwaiq.edu.sa>
2021-10-11 19:39:54 +0300

    nice exercises now if Statement stick in my mind

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,21 @@
 fun main(){
- print("I love Kotlin!!!!")
+ val Player1HealthPoints = 90
+ val Player2HealthPoints = 40
+
+ if(Player1HealthPoints<=90){
+    if(Player1HealthPoints<50) {
+     println("the player's 1 health is awful")
+    }
+    else {
+     println("the player's 1 health is fine")
+    }
+ }
+ if(Player2HealthPoints<=90) {
+  if (Player2HealthPoints<50) {
+      println("the player's 2 health is awful")
+  }
+    else{
+    println("the player's 2 health is fine")
+   }
+ }
 }
