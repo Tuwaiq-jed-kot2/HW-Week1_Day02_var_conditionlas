58b404d831e1fd26f7c1b6d012202069c4689bed
m-ofi <m7mdawfi@gmail.com>
2021-10-11 19:13:30 +0300

    It was nice, I'd have liked it more if it where a bit challenging

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,13 @@
 fun main(){
- print("I love Kotlin!!!!")
+ val firstPlayerHealthPoints = 79
+ val secondPlayerHealthPoints = 45
+
+ if (firstPlayerHealthPoints in 51..90) println("the player's health is fine")
+ else if (firstPlayerHealthPoints < 50) println("the player's health is awful")
+
+ when(secondPlayerHealthPoints){
+  in 90..100 -> println("the player's health is fine")
+  in 0..50 -> println("the player's health is awful")
+  else -> println("Score not valid")
+ }
 }
