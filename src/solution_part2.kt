58769b0bb062e40eb57c4f3ea9cbe58095bb828b
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2021-10-11 19:30:13 +0300

    a

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,16 @@
 fun main(){
- print("I love Kotlin!!!!")
+ val healthPoint1 : Int = 40
+ val healthPoint2 : Int = 90
+
+ if (healthPoint1 in 51..90) {
+     println("Player 1 Health is Fine")
+ } else{
+    println("Player 1 Health is Awful")
+ }
+
+ if (healthPoint2 in 51..90) {
+     println("Player 2 Health is Fine")
+ } else{
+    println("Player 2 Heath is Awful")
+ }
 }
