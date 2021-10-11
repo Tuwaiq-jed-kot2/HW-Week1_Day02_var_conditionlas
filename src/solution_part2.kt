c2e9b044f423c51dda84e2e9b9383388a0261e83
ASUS <EshraqAlmalki>
2021-10-11 22:42:15 +0300

    Q3 made me sure that i need practicing more

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,17 @@
 fun main(){
- print("I love Kotlin!!!!")
+ val player1 = "Me"
+ val healthPointPlayer1 = 12
+ val player2 = "you"
+ val healthPointPlayer2 = 96
+ if(healthPointPlayer1 >= 90){
+  println("the player $player1 health is awful")
+ }else{
+  println("the $player1 health is fine")
+  if(healthPointPlayer2 <=90){
+   println("the $player2 health is fine")
+  }else{
+   println("the $player2 health is awful")
+  }
+ }
+
 }
