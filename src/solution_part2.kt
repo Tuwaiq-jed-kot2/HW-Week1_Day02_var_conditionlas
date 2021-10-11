a8645618378d5fed19527c15dee7e9c77fff71ad
bayanalharthy <Bayan.Mastour.jed.k2@tuwaiq.edu.sa>
2021-10-12 01:28:38 +0300

    good morning ^^

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,24 @@
 fun main(){
- print("I love Kotlin!!!!")
+
+val player1="playerOne"
+ val healthPointPlayer1 = 91
+val player2="playerTwo"
+    val healthPointPlayer2 = 46
+
+if (healthPointPlayer1 >= 90)
+
+    println("the player $player1 health is fin")
+    else
+        println(" the player $player1 health is awful")
+
+   if(healthPointPlayer2 <= 50)
+
+       println(" the player $player2 health is awful")
+
+    else
+       println("the player $player2 health is fin")
+    
+    
+    
+
 }
