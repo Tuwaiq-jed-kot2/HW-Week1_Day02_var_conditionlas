c7278ce615f1d6ea88ec723c79f9042bd81a9ea9
FAISALT1000 <FAISAL.FAHAD.jed.k2@tuwaiq.edu.sa>
2021-10-11 21:16:30 +0300

    thank you for the homework :>

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,38 @@
 fun main(){
- print("I love Kotlin!!!!")
+
+// I used (if) for player 1 and (when) for player 2
+ val player1 = 49
+ val player2 = 101
+
+ println("Player 1 health :")
+ if(player1 <90){
+  if (player1>=50){
+   println("the player's health is fine")
+   }else if(player1>=1) {
+   println("the player's health is awful")
+   }else if(player1==0){
+   println("the player's is DEAD")
+   }else{
+   println("the health value is incorrect")
+   }
+ }else if(player1 <=100){
+  println("the player's health is Graet")
+ }else{
+  println("the health value is incorrect")
+ }
+
+
+
+ println("\nPlayer 2 health :")
+ when (player2) {
+  in 90..100 -> println("the player's health is Graet")
+  in 50..89 -> println("the player's health is fine")
+  in 1..49 ->println("the player's health is awful")
+         0 -> println("the player's is DEAD")
+  else -> println("the health value is incorrect")
+
+
+
+ }
+
 }
