47db7d8ac463cdeeb35f18c5304aa09b0b736078
Qussai <qussai.saeed.jed.k2@tuwaiq.edu.sa>
2021-10-11 19:54:44 +0300

    amazing home work

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,15 @@
 fun main(){
- print("I love Kotlin!!!!")
+ var P1health =60
+ if (P1health >=50)
+ {
+  println (" player1 health is fine")
+ }
+ else { println("the player's health is awful ")
+ }
+ var p2Health = 30
+ if (p2Health <50 )
+  println("the player2 health is awful")
+ else {
+  println ("the player's health is fine")
+ }
 }
