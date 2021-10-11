3c3fa35e9d01c625f6e258f3c56f2ce154ed493d
asus <Asma-Almahdawi>
2021-10-11 23:28:57 +0300

    great كان سهل , خفت شوي من السؤال الثالث رغم اني خلصته بدري بس كنت ادور طرق ثانيه
    اضفت حل ب readline وخليته كومنت
    الله يسعدك ويكتب أجرك

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,40 @@
-fun main(){
- print("I love Kotlin!!!!")
+fun main() {
+
+val healthPointOfPlayerOne = 76
+    val healthPointOfPlayerTwo = 44
+    if (healthPointOfPlayerOne<90) {
+
+        println("the player One  health is fine")
+    }else{
+            println("the player One health is awful")
+    }
+
+        if(healthPointOfPlayerTwo < 50){
+            println("the player Two health is awful")}
+
+        else{
+            println("the player Two health is fine")}
+
 }
+/*
+fun main() {
+
+    println("Enter the health point for first player")
+    val healthPointOfPlayerOne :Int =  readLine()!!.toInt()
+
+    if (healthPointOfPlayerOne<90) {
+
+        println("the player One  health is fine")
+    }else{
+        println("the playerOne health is awful")
+    }
+    println("Enter the health point for Secend player")
+    val healthPointOfPlayerTwo :Int =  readLine()!!.toInt()
+    if (healthPointOfPlayerTwo<50) {
+
+        println("the player One  health is awful")
+    }else{
+        println("the playerOne health is fine")
+    }
+}
+ */
