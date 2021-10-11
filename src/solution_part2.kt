24673dabe7f9e201c8039a6c0b1730be3cd1eb04
AsrarAlbishri <Asrar.Mohammad.jed.k2@tuwaiq.edu.sa>
2021-10-11 23:56:11 +0300

    good exercises :)

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,19 @@
 fun main(){
- print("I love Kotlin!!!!")
+
+
+    val player1healthPoints = 72
+    val player2healthPoints = 22
+
+    if(player1healthPoints in 50..89) {
+        println("The player's health is fine")
+    }else if(player1healthPoints in 0..49){
+        println("The player's health is awful")
+    }
+    if(player2healthPoints in 50..89) {
+        println("The player's health is fine")
+    }else if(player2healthPoints in 0..49){
+        println("The player's health is awful")
+    }
+
+
 }
