56ddfa9cd2d56406a57597f18113c05478626036
RajwaAbdulaziz <Rajwa.Abdulaziz.jed.k2@tuwaiq.edu.sa>
2021-10-11 21:55:41 +0300

    I need more exercises to apply all I learned.

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,33 @@
 fun main(){
- print("I love Kotlin!!!!")
+
+ val player1 = "Rajwa"
+ val player2 = "Sara"
+
+ val player1HealthPoint = 101
+ val player2HealthPoint = 103
+
+ if(player1HealthPoint <= 100) {
+
+  when (player1HealthPoint) {
+   in 90..100 -> println("$player1's health is excellent")
+   in 50..89 -> println("$player1's health is fine")
+   else -> println("$player1's health is awful")
+  }
+
+ } else {
+  println("Player's health points can't exceed 100")
+ }
+
+ if(player2HealthPoint <= 100) {
+
+  when (player2HealthPoint) {
+   in 90..100 -> println("$player2's health is excellent")
+   in 50..89 -> println("$player2's health is fine")
+   else -> println("$player2's health is awful")
+  }
+
+ } else {
+  println("Player's health points can't exceed 100")
+ }
+
 }
