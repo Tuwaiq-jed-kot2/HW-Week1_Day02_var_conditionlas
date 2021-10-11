8768cdc3953bce52dcbeb4e331ab3ae5e537675f
Hind Zaki <Hind.Abdulbari.jed.k2@tuwaiq.edu.sa>
2021-10-11 23:16:51 +0300

    it was easy

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,9 @@
-fun main(){
- print("I love Kotlin!!!!")
+fun main() {
+ val healthPointP1 = 94
+ val healthPointP2 = 45
+ if (healthPointP1 >= 90) {
+  println("you are fine")
+  if (healthPointP2 <= 50)
+   println("you need to see a Doctor!!!")
+ }
 }
