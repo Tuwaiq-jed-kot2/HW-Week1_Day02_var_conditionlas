1b1fe6d71943e4d2b91bd004242c840b536f2df4
Sirin Alkadi <serenalgadi@gmail.com>
2021-10-12 04:53:56 +0300

    Interesting

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,28 @@
 fun main(){
- print("I love Kotlin!!!!")
+    val playerOne = 80
+    val playerTwo = 50
+    val playerOneName = "Ahmed"
+    val playerTwoName = "Khaled"
+
+    if (playerOne  > 50) {
+        println("The player's health is fine")
+    }else{
+        println("The player's health is awful")
+    }
+    if (playerTwo  > 50) {
+        println("The player's health is fine")
+    }else{
+        println("The player's health is awful")
+    }
+//حل ثاني
+    if (playerOne in 51..90) {
+        println("$playerOneName's health is fine")
+    }else if (playerOne <= 50){
+        println("$playerOneName's health is awful")
+    }
+    if (playerTwo in 51..90) {
+        println("$playerTwoName's health is fine")
+    }else if (playerTwo <= 50){
+        println("$playerTwoName's health is awful")
+    }
 }
