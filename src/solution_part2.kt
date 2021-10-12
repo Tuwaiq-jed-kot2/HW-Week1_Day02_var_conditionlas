472d3c55a3228e298d8175d455fd711bd231a9f9
Nayef Saeed Alzahrani <NAYEF.SAEED.jed.k2@tuwaiq.edu.sa>
2021-10-12 03:37:04 +0300

    Everything is okay

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,21 @@
 fun main(){
- print("I love Kotlin!!!!")
+ val firstPlayer = 200
+ val firstPlayerHealthPoint = when(firstPlayer){
+  in 91..100 -> "The player's health is excellent"
+  in 50..90 -> "The player's health is fine"
+  in 20..49 -> "The player's health is awful"
+  in 0..19 -> "Dying"
+  else -> "Exceptional case"
+ }
+ println(firstPlayerHealthPoint)
+
+ val secondPlayer = 49
+ val secondPlayerHealthPoint = when(secondPlayer){
+  in 91..100 -> "The player's health is excellent"
+  in 50..90 -> "The player's health is fine"
+  in 20..49 -> "The player's health is awful"
+  in 0..19 -> "Dying"
+  else -> "Exceptional case"
+ }
+ println(secondPlayerHealthPoint)
 }
