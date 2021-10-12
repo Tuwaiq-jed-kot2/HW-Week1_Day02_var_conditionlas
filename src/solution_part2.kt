eef25d8ed41a2149d0b9f563040823c24c9ace37
irakan4 <irakan4>
2021-10-12 12:48:45 +0300

    q2 has been solved!! q3 has been solved!!

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,14 +1,18 @@
-fun main(){
+fun main() {
 
- val age:Int = 19
+ val player1 = "PlayerOne"
+ val healthPointPlayer1 = 91
+ val player2 = "PlayerTwo"
+ val healthPointPlayer2 = 49
 
- if(age < 18 ) {
-  print("you are not adult")
- }else {
-  print("you are adult")
- }
+ if (healthPointPlayer1 >= 90)
 
+  println("The player $player1 health is fine")
+ else
+  print(" The player $player1 health is awful ")
 
+ if (healthPointPlayer2 >= 50)
+  println(" The Player $player2 health is fine")
+ else
+  println("the player $player2 health is awful")
 }
-
-
