2d3294816246e73524241dad5524a62ebbe354e6
ranaowaidhi <ranaowaidhi@gmail.com>
2021-10-12 04:31:32 +0300

    I made huge mistake in the first commit, but I noticed it , and I fixed it, I'm not sure about how many attempt allowed but I hope that's fine :)

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,15 +1,15 @@
 fun main() {
  val player1 = "Ahmed"
  val player2 = "Mohamed"
- val healthPointsP1 = 91
+ val healthPointsP1 = 79
  val healthPointsP2 = 22
 
- if (healthPointsP1 in 90..100){
+ if (healthPointsP1 in 50..89){
   println("$player1 health's is fine")
  } else if (healthPointsP1 < 50){
   println("$player1 health's is awful")
  }
- if (healthPointsP2 in 90..100){
+ if (healthPointsP2 in 50..89){
   println("$player2 health's is fine")
  } else if (healthPointsP2 < 50){
   println("$player2 health's is awful")
