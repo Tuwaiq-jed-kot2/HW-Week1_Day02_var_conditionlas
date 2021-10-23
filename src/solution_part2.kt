8543bcabcd7902f4985c176f583b216d0939e4ad
Nayef Saeed Alzahrani <NAYEF.SAEED.jed.k2@tuwaiq.edu.sa>
2021-10-23 19:55:43 +0300

    Everything is okay

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,5 +1,5 @@
 fun main(){
- val firstPlayer = 200
+ val firstPlayer = 90
  val firstPlayerHealthPoint = when(firstPlayer){
   in 91..100 -> "The player's health is excellent"
   in 50..90 -> "The player's health is fine"
@@ -11,8 +11,8 @@ fun main(){
 
  val secondPlayer = 49
  val secondPlayerHealthPoint = when(secondPlayer){
-  in 91..100 -> "The player's health is excellent"
-  in 50..90 -> "The player's health is fine"
+  in 90..100 -> "The player's health is excellent"
+  in 50..89 -> "The player's health is fine"
   in 20..49 -> "The player's health is awful"
   in 0..19 -> "Dying"
   else -> "Exceptional case"
