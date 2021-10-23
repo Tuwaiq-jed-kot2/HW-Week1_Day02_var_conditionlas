8e9f7c82c10eae71d9f71e1f3e2b71962f4c10d1
ASUS <EshraqAlmalki>
2021-10-23 19:45:16 +0300

    Q3 made me sure that i need practicing more completed..

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,17 +1,24 @@
-fun main(){
+fun main() {
  val player1 = "Me"
- val healthPointPlayer1 = 12
+ val healthPointPlayer1 = 77
  val player2 = "you"
  val healthPointPlayer2 = 96
- if(healthPointPlayer1 >= 90){
-  println("the player $player1 health is awful")
- }else{
+ if(healthPointPlayer1 in 91..100){
+  println("the $player1 health is good")
+ }else if (healthPointPlayer1 in 51..90 ){
   println("the $player1 health is fine")
-  if(healthPointPlayer2 <=90){
-   println("the $player2 health is fine")
-  }else{
-   println("the $player2 health is awful")
-  }
+ }else if (healthPointPlayer1 in 1..50){
+  println("the $player1 health is awful")
+ }else{
+  println("GAME OVER")
  }
-
+ if(healthPointPlayer2 in 91..100){
+  println("the $player2 health is good")
+ }else if (healthPointPlayer2 in 51..90 ){
+  println("the $player2 health is fine")
+ }else if (healthPointPlayer2 in 1..50){
+  println("the $player2 health is awful")
+ }else{
+  println("GAME OVER")
+ }
 }
